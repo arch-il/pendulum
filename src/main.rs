554a0547a1fac970b3f4a49dc633bfe0db5d0371
arch_il <archilberaia+github@gmail.com>
2025-01-05 17:22:07 +0400

    test line drawing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 use macroquad::prelude::*;
+use pendulum::Pendulum;
+
+mod pendulum;
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
+    let pendulum = Pendulum::new();
+
     loop {
-        clear_background(RED);
+        clear_background(BLACK);
+        draw_line(pendulum.p1.x, pendulum.p1.y, pendulum.p2.x, pendulum.p2.y, 5.0, LIME);
+        draw_line(pendulum.p2.x, pendulum.p2.y, pendulum.p3.x, pendulum.p3.y, 5.0, LIME);
+        
+        // draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        // draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
-
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+        // draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
 
         next_frame().await
     }
