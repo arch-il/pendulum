55e22671786ce1d26f9cb91ca4057122b2d6161e
arch_il <archilberaia+github@gmail.com>
2025-01-05 17:28:47 +0400

    setup environment

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,46 @@
-use macroquad::prelude::*;
+use macroquad::{color, shapes::draw_line, window};
 use pendulum::Pendulum;
 
 mod pendulum;
 
-#[macroquad::main("BasicShapes")]
+fn window_conf() -> window::Conf {
+    window::Conf {
+        window_title: "Pendulum Simulation".to_owned(),
+        window_resizable: false,
+        window_width: 500,
+        window_height: 500,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
 async fn main() {
     let pendulum = Pendulum::new();
 
     loop {
-        clear_background(BLACK);
-        draw_line(pendulum.p1.x, pendulum.p1.y, pendulum.p2.x, pendulum.p2.y, 5.0, LIME);
-        draw_line(pendulum.p2.x, pendulum.p2.y, pendulum.p3.x, pendulum.p3.y, 5.0, LIME);
-        
+        window::clear_background(color::BLACK);
+        draw_line(
+            pendulum.p1.x,
+            pendulum.p1.y,
+            pendulum.p2.x,
+            pendulum.p2.y,
+            5.0,
+            color::LIME,
+        );
+        draw_line(
+            pendulum.p2.x,
+            pendulum.p2.y,
+            pendulum.p3.x,
+            pendulum.p3.y,
+            5.0,
+            color::LIME,
+        );
+
         // draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
         // draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
 
         // draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
 
-        next_frame().await
+        window::next_frame().await
     }
 }
--- src/pendulum.rs
+++ src/pendulum.rs
@@ -1,17 +1,17 @@
 use macroquad::math::Vec2;
 
 pub struct Pendulum {
-	pub p1: Vec2,
-	pub p2: Vec2,
-	pub p3: Vec2,
+    pub p1: Vec2,
+    pub p2: Vec2,
+    pub p3: Vec2,
 }
 
 impl Pendulum {
-	pub fn new() -> Self {
-		Self {
-			p1: Vec2::new(0.0, 0.0),
-			p2: Vec2::new(100.0, 100.0),
-			p3: Vec2::new(200.0, 100.0),
-		}
-	}
+    pub fn new() -> Self {
+        Self {
+            p1: Vec2::new(0.0, 0.0),
+            p2: Vec2::new(100.0, 100.0),
+            p3: Vec2::new(200.0, 100.0),
+        }
+    }
 }
